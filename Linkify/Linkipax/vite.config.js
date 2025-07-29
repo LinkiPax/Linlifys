10d96f685edbafa2145feb35d97df0283f529192
Linkipax <linkipax@gmail.com>
2025-07-30 02:25:14 +0530

    yo

--- Linkify/Linkipax/vite.config.js
+++ Linkify/Linkipax/vite.config.js
@@ -1,32 +1,4 @@
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
 
-// // // https://vite.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// //   optimizeDeps: {
-// //     include: ['@emotion/react', '@emotion/styled'],
-// //   }
-// // })
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import fs from 'fs'
-
-// export default defineConfig({
-  
-//   plugins: [react()],
-//   define: {
-//     global: 'window',
-//   },
-//   server: {
-//     https: {
-//       key: fs.readFileSync('./localhost+1-key.pem'),
-//       cert: fs.readFileSync('./localhost+1.pem'),
-//     },
-//     host: '0.0.0.0',
-//     port: 5173
-//   }
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
