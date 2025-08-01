b406afcd6471c4932943179729f44e34f6c575f9
Linkipax <linkipax@gmail.com>
2025-08-01 06:03:14 +0530

    yo

--- Linkify/Linkipax/src/component/MyNetwork/Mynewwork.jsx
+++ Linkify/Linkipax/src/component/MyNetwork/Mynewwork.jsx
@@ -277,8 +277,7 @@ const MyNetwork = () => {
                               }
                               alt="Profile"
                               className="rounded-circle mb-3 profile-image-my"
-                              width="100"
-                              height="100"
+                              
                             />
                             <Badge
                               bg="success"
