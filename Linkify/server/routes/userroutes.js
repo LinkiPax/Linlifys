fecbbfb23397f990ce2ec53bbce5f770276c0ea8
Linkipax <linkipax@gmail.com>
2025-10-14 01:55:14 +0530

    yo

--- Linkify/server/routes/userroutes.js
+++ Linkify/server/routes/userroutes.js
@@ -602,7 +602,7 @@ router.get('/google/callback',
                 setCookie(res, token);
             } else {
                 // redirectUrl = `${clientURL}/home/?token=${token}&user=${userDataString}&auth=success&id=${req.user._id}`;
-                redirectUrl = `${clientURL}/home/${req.user._id}`;
+                redirectUrl = `${clientURL}/home/${req.user._id}?token=${token}&user=${userDataString}&auth=success`;
                 setCookie(res, token);
             }
             
