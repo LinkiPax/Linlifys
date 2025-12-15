ed1038883115112a4b5cfb7b6f313fc3448e3f53
Linkipax <linkipax@gmail.com>
2025-12-15 17:26:28 +0530

    lets do

--- Linkify/server/server.js
+++ Linkify/server/server.js
@@ -204,7 +204,7 @@ app.use(cors({
   origin: FRONTEND_ORIGIN,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept','x-user-id' ],
 }));
 console.log(`✅ CORS Enabled for: ${FRONTEND_ORIGIN}`);
 
