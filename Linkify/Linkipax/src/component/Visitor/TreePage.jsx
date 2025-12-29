7ee31f4a0eae7e121e082ddf61b068be218fc704
Linkipax <linkipax@gmail.com>
2025-12-30 03:18:41 +0530

    Ab dekho

--- Linkify/Linkipax/src/component/Visitor/TreePage.jsx
+++ Linkify/Linkipax/src/component/Visitor/TreePage.jsx
@@ -63,7 +63,7 @@ import { registerVisit } from "./visitor";
 
 const INITIAL_OFFSETS = [
   { x: -3598,   y: -37.5, z: -37.5, scale: 0.17 },
-  { x: -2893.5, y: 138.5, z: -12,    scale: 0.17 },
+  { x: -2893.5, y: 138.5, z: -40,    scale: 0.17 },
   { x: -2082.5, y: 87,    z: -12,    scale: 0.17 },
   { x: -1045.5, y: -42.5, z: -12,    scale: 0.17 },
   { x: 186.5,   y: -10.5, z: -12,    scale: 0.17 },
