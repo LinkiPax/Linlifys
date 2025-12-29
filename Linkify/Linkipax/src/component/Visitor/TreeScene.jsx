be1879aa900ac4fda6855af289133e242517b0b1
Linkipax <linkipax@gmail.com>
2025-12-29 17:50:37 +0530

    go

--- Linkify/Linkipax/src/component/Visitor/TreeScene.jsx
+++ Linkify/Linkipax/src/component/Visitor/TreeScene.jsx
@@ -1,33 +1,36 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment, Grid } from "@react-three/drei";
 import TreeGrowth from "./TreeGrowth";
+import TreeDebugUI from "./TreeDebugUI";
 
 export default function TreeScene() {
   return (
-    <Canvas
-      camera={{
-        position: [0, 6, 18],
-        fov: 40,
-        near: 0.1,
-        far: 300
-      }}
-      style={{ background: "#eaf7ff" }}
-    >
-      {/* 💡 LIGHT */}
-      <ambientLight intensity={0.7} />
-      <directionalLight position={[5, 10, 5]} intensity={1.2} />
+    <div style={{ width: "100vw", height: "100vh", position: "relative" }}>
+      
+      {/* 🌳 DEBUG UI (HTML → OUTSIDE CANVAS) */}
+      <TreeDebugUI />
 
-      {/* 📐 DEBUG HELPERS */}
-      <axesHelper args={[5]} />
-      <Grid args={[40, 40]} />
+      {/* 🎮 THREE.JS CANVAS */}
+      <Canvas
+        camera={{
+          position: [0, 6, 18],
+          fov: 40,
+          near: 0.1,
+          far: 300
+        }}
+        style={{ background: "#eaf7ff" }}
+      >
+        <ambientLight intensity={0.7} />
+        <directionalLight position={[5, 10, 5]} intensity={1.2} />
 
-      {/* 🌳 TREE */}
-      <TreeGrowth />
+        <axesHelper args={[5]} />
+        <Grid args={[40, 40]} />
 
-      {/* 🎥 CAMERA */}
-      <OrbitControls target={[0, 3, 0]} maxPolarAngle={Math.PI / 2.2} />
+        <TreeGrowth />
 
-      <Environment preset="forest" />
-    </Canvas>
+        <OrbitControls target={[0, 3, 0]} maxPolarAngle={Math.PI / 2.2} />
+        <Environment preset="forest" />
+      </Canvas>
+    </div>
   );
 }
